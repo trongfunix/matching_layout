4fa53e3008821052f0cc3258238fcc1e2e51d00c
trongfunix <trongtdfx18290@funix.edu.vn>
2023-12-22 09:43:32 +0700

    update js

--- main.js
+++ main.js
@@ -1,63 +1,46 @@
-// Lấy danh sách các mục có thể kéo
-const draggables = document.querySelectorAll(".right_content");
-let dragSrcEl = null;
+const rightItem = document.querySelectorAll(".right_content_item");
+const rightItemBtn = document.querySelectorAll(".right_content_item .btn");
 
-console.log("draggables", draggables);
+console.log("rightItem", rightItem);
+console.log("rightItemBtn", rightItemBtn);
 
-function handleDragStart(e) {
-  console.log("drag start");
-  this.style.opacity = "0.4";
-  dragSrcEl = this;
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/html", this.innerHTML);
-}
+let draggedItem = null;
 
-function handleDragOver(e) {
-  console.log("drag over");
-  if (e.preventDefault) {
-    e.preventDefault(); // Ngăn chặn sự kiện mặc định để cho phép thả
-  }
-  return false;
-}
-
-function handleDragEnter() {
-  console.log("drag enter");
-  this.classList.add("over");
-}
-
-function handleDragLeave() {
-  console.log("drag leave");
-  this.classList.remove("over");
-}
-
-function handleDrop(e) {
-  console.log("drag drop");
-  if (e.stopPropagation) {
-    e.stopPropagation(); // Ngăn chặn sự kiện "drop" từ lan sang ngoài
-  }
-
-  if (dragSrcEl !== this) {
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData("text/html");
-  }
-
-  return false;
-}
-
-function handleDragEnd() {
-  console.log("drag end");
-  this.style.opacity = "1";
-  draggables.forEach(function (draggable) {
-    draggable.classList.remove("over");
+// xử lý khi bắt đầu kéo các item button
+rightItemBtn.forEach((btn) => {
+  btn.addEventListener("dragstart", function (e) {
+    // khi bắt đầu kéo thì sẽ lấy nội dung của button đó lưu lại
+    draggedItem = btn.innerHTML;
   });
-}
 
-// Thêm các sự kiện kéo và thả cho mỗi mục có thể kéo
-draggables.forEach(function (draggable) {
-  draggable.addEventListener("dragstart", handleDragStart);
-  draggable.addEventListener("dragenter", handleDragEnter);
-  draggable.addEventListener("dragover", handleDragOver);
-  draggable.addEventListener("dragleave", handleDragLeave);
-  draggable.addEventListener("drop", handleDrop);
-  draggable.addEventListener("dragend", handleDragEnd);
+  btn.addEventListener("dragend", function (e) {
+    // khi kết thúc kéo thì sẽ gán lại nội dung của chỗ đã thả cho button đó
+    console.log("dragend", draggedItem);
+    btn.innerHTML = draggedItem;
+    draggedItem = null;
+  });
+});
+
+// xử lý khi kéo dragover và thả vào khối container bên phải
+rightItem.forEach((item) => {
+  item.addEventListener("dragover", function (e) {
+    // console.log("dragover", item);
+    e.preventDefault();
+  });
+
+  // xử lý khi thả vào khối container bên phải
+  item.addEventListener("drop", function (e) {
+    console.log("dragstart", draggedItem);
+
+    console.log("drop", item.querySelector(".btn").innerHTML);
+
+    // cần clone lại nội dung trước khi thả
+    e.preventDefault();
+    const cloneItem = item.querySelector(".btn").innerHTML;
+
+    item.querySelector(".btn").innerHTML = draggedItem;
+
+    // gán lại nội dung đã clone để sử dụng khi kéo kết thúc
+    draggedItem = cloneItem;
+  });
 });
